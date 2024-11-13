097a6f3b579e714a5fd81df4c4ea4ac9af56d8f9
Gene Bogdanovich <genebogdanovich@gmail.com>
2024-11-13 09:59:26 +0300

    Initial Commit

---
+++ RuleMarkAnnotationWithChartScrollableAxes/RuleMarkAnnotationWithChartScrollableAxesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RuleMarkAnnotationWithChartScrollableAxesApp.swift
+//  RuleMarkAnnotationWithChartScrollableAxes
+//
+//  Created by Gene Bogdanovich on 13.11.24.
+//
+
+import SwiftUI
+
+@main
+struct RuleMarkAnnotationWithChartScrollableAxesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
